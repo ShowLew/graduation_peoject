c7e55b5dab59900c9f90e690d7d53ef2c84020ec
showlew <2277499890@qq.com>
2018-05-04 12:09:44 +0800

    http base

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def authenticate_admin_user!
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == Keys.admin.user_name && password == Keys.admin.password
+    end
+  end
 end
